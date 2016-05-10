eaf66d13e8406d3d974fe5df6d96ac482d158d94
Fizal Sarif <fizal.sarif@gmail.com>
2016-05-10 13:58:18 -0400

    fix indentation error

--- scraper.py
+++ scraper.py
@@ -7,10 +7,10 @@ place= sys.argv[2]
  
 
 # read API keys
- with io.open('yelp.json') as cred:
-     creds = json.load(cred)
-     auth = Oauth1Authenticator(**creds)
-     client = Client(auth)
+with io.open('yelp.json') as cred:
+	creds = json.load(cred)
+	auth = Oauth1Authenticator(**creds)
+	client = Client(auth)
 
 
 # make request for the search term store in array
@@ -23,7 +23,7 @@ params = {
 results = client.search(place, **params)
 
 print(results.total + " results for " + term + " in " + place)
-
+print(results.business[0].name + " is the first item on the list.")
 
 # iterate array and extract 
 
